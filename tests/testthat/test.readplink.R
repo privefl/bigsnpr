d363d2464b84e86d10f736d65ecdd50c042716f9
Florian <florian.prive.21@gmail.com>
2016-07-21 00:02:55 +0200

    test part

--- tests/testthat/test.readplink.R
+++ tests/testthat/test.readplink.R
@@ -1,5 +1,5 @@
 ################################################################################
-#
+
 context("READPLINK")
 
 if (!dir.exists("backingfiles")) dir.create("backingfiles")
@@ -29,32 +29,32 @@ test_that("data.frames of same size", {
 
 ################################################################################
 
-if (!dir.exists("backingfiles")) dir.create("backingfiles")
-if (file.exists("backingfiles/test2.bk")) file.remove("backingfiles/test2.bk")
-
-pedfile <- system.file("extdata", "example.ped.gz", package = "bigsnpr")
-
-test3 <- PedToBig(pedfile, 50, "test2", "backingfiles")
-
-test_that("Same types in data.frames", {
-  expect_equal(sapply(test$fam, typeof), sapply(test3$fam, typeof))
-  expect_equal(sapply(test$map, typeof), sapply(test3$map, typeof))
-})
-
-test_that("good class", {
-  expect_match(class(test3), "bigSNP")
-})
-
-test_ref <- function(code) {
-  check1 <- all(code %in% c(2, 20, 11, NA))
-  check2 <- all(code %in% c(0, 11, 22, NA))
-
-  check1 | check2
-}
-
-code.gen <- 10 * test$genotypes[,] + test3$genotypes[,]
-check <- apply(code.gen, 2, test_ref)
-
-test_that("Same genotypes depending on NA/ref", {
-  expect_true(all(check))
-})
+# if (!dir.exists("backingfiles")) dir.create("backingfiles")
+# if (file.exists("backingfiles/test2.bk")) file.remove("backingfiles/test2.bk")
+#
+# pedfile <- system.file("extdata", "example.ped.gz", package = "bigsnpr")
+#
+# test3 <- PedToBig(pedfile, 50, "test2", "backingfiles")
+#
+# test_that("Same types in data.frames", {
+#   expect_equal(sapply(test$fam, typeof), sapply(test3$fam, typeof))
+#   expect_equal(sapply(test$map, typeof), sapply(test3$map, typeof))
+# })
+#
+# test_that("good class", {
+#   expect_match(class(test3), "bigSNP")
+# })
+#
+# test_ref <- function(code) {
+#   check1 <- all(code %in% c(2, 20, 11, NA))
+#   check2 <- all(code %in% c(0, 11, 22, NA))
+#
+#   check1 | check2
+# }
+#
+# code.gen <- 10 * test$genotypes[,] + test3$genotypes[,]
+# check <- apply(code.gen, 2, test_ref)
+#
+# test_that("Same genotypes depending on NA/ref", {
+#   expect_true(all(check))
+# })
