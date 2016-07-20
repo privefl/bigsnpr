e59d4ba09252511f1b4dd3457f3fe0fd14a8a2bb
Florian <florian.prive.21@gmail.com>
2016-07-20 11:49:49 +0200

    add AUC + GBLUP

---
+++ R/AUC.R
@@ -0,0 +1,73 @@
+################################################################################
+
+#' @title AUC.
+#' @description Compute the Area Under the ROC Curve (AUC) of a predictor
+#' and possibly its 95\% confidence interval.
+#' @details Other packages provide ways to compute the AUC.
+#' Yet, I found out that when there are equalities, those can
+#' overestimate the true AUC. So, I choose to compute the AUC
+#' through an (accurate) estimation of its statistical definition as a
+#' probability: \deqn{\P{score(x_{case}) > score(x_{control})}}
+#' @return The AUC, a probability, and possibly its 2.5\% and 97.5\% quantiles
+#' (95\% CI).
+#' @param pred Vector of predictions.
+#' @param y Vector of true values (for the same individuals).
+#' @param nboot Number of bootstrap samples to evaluate 95\% CI.
+#' @param nsim Number of pairs (case/control) that are compared
+#' in order to approximate the probability.
+#' @param seed See \code{\link{set.seed}}. Use it for reproducibility.
+#' @param digits See \code{\link{round}}.
+#' @seealso pROC::\code{\link[pROC]{auc}} \cr
+#' AUC::\code{\link[AUC]{auc}} \cr
+#' Hmisc::\code{\link[Hmisc]{somers2}}
+#' @references Tom Fawcett. 2006. An introduction to ROC analysis.
+#' Pattern Recogn. Lett. 27, 8 (June 2006), 861-874.
+#' DOI=\url{http://dx.doi.org/10.1016/j.patrec.2005.10.010}.
+#' @name auc
+NULL
+
+################################################################################
+
+#' @name AucSample
+#' @rdname auc
+#' @export
+AucSample <- function(pred, y, nsim = 1e7, seed = NULL, digits = 3) {
+  pred.case    <- pred[y == 1]
+  pred.control <- pred[y == -1]
+
+  set.seed(seed)
+
+  pred.case.sample    <- sample(pred.case,    nsim, replace = TRUE)
+  pred.control.sample <- sample(pred.control, nsim, replace = TRUE)
+
+  return(round(mean(pred.case.sample > pred.control.sample), digits))
+}
+
+################################################################################
+
+#' @name AucSampleConf
+#' @rdname auc
+#' @export
+AucSampleConf <- function(pred, y, nboot = 1e5, nsim = 1e4,
+                         seed = NULL, digits = 3) {
+  pred.case    <- pred[y == 1]
+  pred.control <- pred[y == -1]
+
+  set.seed(seed)
+
+  sampleRes <- function() {
+    ind.case <- sample(length(pred.case), replace = TRUE)
+    ind.control <- sample(length(pred.control), replace = TRUE)
+    pred.case.sample    <- sample(pred.case[ind.case],
+                                  nsim, replace = TRUE)
+    pred.control.sample <- sample(pred.control[ind.control],
+                                  nsim, replace = TRUE)
+    mean(pred.case.sample > pred.control.sample)
+  }
+
+  res <- replicate(nboot, sampleRes())
+  q <- quantile(res, c(0.025, 0.975))
+  return(round(c("Mean" = mean(res), q), digits))
+}
+
+################################################################################
---
+++ examples/example.GBLUP.R
@@ -0,0 +1,23 @@
+bedfile <- system.file("extdata", "example.bed", package = "bigsnpr")
+
+# Creating directory for backing files
+if (!dir.exists("backingfiles")) dir.create("backingfiles")
+
+if (!file.exists("backingfiles/test_doc.bk")) {
+  # Reading the bedfile and storing the data
+  test <- BedToBig(bedfile, 50, "test_doc")
+} else {
+  # Loading it from backing files
+  test <- AttachBigSNP("test_doc")
+}
+
+# create some ramdon class
+test$fam$affection <- sample(c(-1, 1),
+                             length(test$fam$affection),
+                             replace = TRUE)
+
+# Get predictions from half the data
+n <- nrow(test$genotypes)
+ind.train <- sort(sample(n, n/2))
+test2 <- GBLUP(test, block.size = 1000, ind.train = ind.train)
+print(AucSampleConf(test2, test$fam$affection[-ind.train], nboot = 1e4))
