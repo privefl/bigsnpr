882b84331021b61368fa14e4ee3d4639503faa63
Florian <florian.prive.21@gmail.com>
2016-08-01 18:27:40 +0200

    without parallelism

--- tests/testthat/test.pruning.R
+++ tests/testthat/test.pruning.R
@@ -17,15 +17,15 @@ test_that("Phenotypes needs to be defined", {
 test$fam$pheno <- sample(c(-1, 1), size = nrow(test$fam),
                          replace = TRUE)
 ind <- Prune(test)
-ind2 <- Prune(test, ncores = 2)
+# ind2 <- Prune(test, ncores = 2)
 ind3 <- Prune(test, thr.pvalue = 2)
 ind4 <- Prune(test, thr.corr = 0.5)
 
 ################################################################################
 
-test_that("Same results with parallelism", {
-  expect_equal(ind, ind2)
-})
+# test_that("Same results with parallelism", {
+#   expect_equal(ind, ind2)
+# })
 
 test_that("Lengths differ with thresholds", {
   expect_gte(length(ind), length(ind3))
