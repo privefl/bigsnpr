d50e4810ca9ea9bacda9f852ef5523ff66cc88c9
Florian Privé <florian.prive.21@gmail.com>
2017-03-23 22:33:56 +0100

    ggplot in bigstatsr

--- R/pcadapt.R
+++ R/pcadapt.R
@@ -27,28 +27,6 @@ snp_qq(tmp)
 snp_qq(snp_gc(tmp))
 snp_manhattan(snp_gc(tmp), popres$map)
 
-plot.big_SVD <- function(x, type = c("screeplot", "scores", "loadings"),
-                         nval = length(x$d),
-                         scores = c(1, 2),
-                         loading = 1,
-                         pch = 19, cex = 0.5,
-                         ...) {
 
-  type <- match.arg(type)
-  if (type == "screeplot") {
-    plot(x$d[seq_len(nval)], type = "b", ylab = "singular value",
-         pch = pch, cex = cex, ...)
-  } else if (type == "scores") {
-    plot(predict(x)[, scores],
-         xlab = paste0("PC", scores[1]),
-         ylab = paste0("PC", scores[2]),
-         pch = pch, cex = cex, ...)
-  } else if (type == "loadings") {
-    plot(x$v[, loading], ylab = paste0("loadings of PC", loading),
-         pch = pch, cex = cex, ...)
-  }
-}
-plot(svd, nval = 10)
-plot(svd, type = "scores", scores = 3:4)
-plot(svd, type = "loadings", loading = 1)
+
 
--- tmp-tests/privefl.R
+++ tmp-tests/privefl.R
@@ -2,6 +2,10 @@ require(bigsnpr)
 require(pcadapt)
 
 test <- snp_attachExtdata()
+files <- system.file("extdata", paste0("cluster", 1:3), package = "bigsnpr")
+test <- snp_getPops(test, pop.files = files,
+                    col.sample.ID = 2,
+                    col.family.ID = 3)
 mat <- t(attach.BM(test$genotypes)[,])
 
 svd <- big_SVD(test$genotypes, snp_scaleBinom(), k = 10)
@@ -10,8 +14,35 @@ x <- pcadapt(mat, K = 10)
 plot(x,option="scores")
 plot(svd)
 
+require(ggplot2)
+scores <- predict(svd)
+p <- qplot(x = scores[, 1], y = scores[, 2])
+population <- test$fam$family.ID
+
+
+
+MYTHEME <- function(p, title = NULL, coeff = 1) {
+  p + theme_bw() + ggtitle(title) +
+    theme(plot.title   = element_text(size = rel(2.0 * coeff), hjust = 0.5),
+          legend.title = element_text(size = rel(1.5 * coeff)),
+          legend.text  = element_text(size = rel(1.2 * coeff)),
+          axis.title   = element_text(size = rel(1.5 * coeff)),
+          axis.text    = element_text(size = rel(1.2 * coeff)))
+}
+
+population <- test$fam$family.ID
+MYTHEME(qplot(x = scores[, 1], y = scores[, 2]),
+        title = "Scores of PCA",
+        coeff = 1.2) + xlab("PC1") + ylab("PC2") +
+  theme(legend.position = c(0.83, 0.15)) + geom_point(aes(color = population))
+
+nval <- 97
+
+qplot(y = svd$v[, 1])
+
 
 x <- pcadapt(mat, K = 3)
+require(robust)
 tmp <- snp_pcadapt(test$genotypes, svd$u[, 1:3])
 plot(x$stat, tmp[[1]])
 plot(x$pvalues, predict(snp_gc(tmp)), log = "xy")
