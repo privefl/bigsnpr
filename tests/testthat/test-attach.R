3ebee620832aed3cd92c224a0435e832ce8dcbb7
Florian <florian.prive.21@gmail.com>
2017-08-30 13:02:54 +0200

    try again

--- tests/testthat/test-attach.R
+++ tests/testthat/test-attach.R
@@ -23,7 +23,7 @@ expect_error(snp_attach(rdsfile.copy))  # File doesn't exist
 expect_true(file.copy(bkfile, bkfile.copy))
 test3 <- snp_attach(rdsfile.copy)
 expect_null(test3$savedIn)
-expect_equal(test3$genotypes$backingfile, bkfile.copy)
+expect_equal(test3$genotypes$backingfile, normalizePath(bkfile.copy))
 expect_s4_class(test3$genotypes, "FBM.code256")
 
 ################################################################################
