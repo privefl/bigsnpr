96d43e2abe6561931dfac22afb2e98d5fd2bf5ef
privefl <florian.prive.21@gmail.com>
2018-04-10 13:51:32 +0200

    add faster options

--- R/impute.R
+++ R/impute.R
@@ -5,7 +5,7 @@ CODE_IMPUTE_PRED  <- c(0, 1, 2, NA, 0, 1, 2, rep(NA, 249))
 
 ################################################################################
 
-imputeChr <- function(Gna, ind.chr, alpha, size, p.train, seed) {
+imputeChr <- function(Gna, ind.chr, alpha, size, p.train, n.cor, seed) {
 
   # reproducibility
   if (!any(is.na(seed))) set.seed(seed[attr(ind.chr, "chr")])
@@ -19,7 +19,7 @@ imputeChr <- function(Gna, ind.chr, alpha, size, p.train, seed) {
   # correlation between SNPs
   corr <- snp_cor(
     Gna = X,
-    ind.row = 1:n,
+    ind.row = sort(sample(n.cor)),
     ind.col = ind.chr,
     size = size,
     alpha = alpha,
@@ -69,12 +69,14 @@ imputeChr <- function(Gna, ind.chr, alpha, size, p.train, seed) {
 #' has not been extensively compared with other imputation methods yet.**
 #'
 #' @inheritParams bigsnpr-package
-#' @param alpha Type-I error for testing correlations. Default is `0.02`.
+#' @param alpha Type-I error for testing correlations. Default is `1e-4`.
 #' @param size Number of neighbor SNPs to be possibly included in the model
-#' imputing this particular SNP. Default is `500`.
+#'   imputing this particular SNP. Default is `200`.
 #' @param p.train Proportion of non missing genotypes that are used for training
-#' the imputation model while the rest is used to assess the accuracy of
-#' this imputation model. Default is `0.8`.
+#'   the imputation model while the rest is used to assess the accuracy of
+#'   this imputation model. Default is `0.8`.
+#' @param n.cor Number of rows that are used to estimate correlations.
+#'   Default uses them all.
 #' @param seed An integer, for reproducibility. Default doesn't use seeds.
 #'
 #' @return A `data.frame` with
@@ -85,9 +87,10 @@ imputeChr <- function(Gna, ind.chr, alpha, size, p.train, seed) {
 #' @import Matrix xgboost
 #'
 snp_fastImpute <- function(Gna, infos.chr,
-                           alpha = 0.02,
-                           size = 500,
+                           alpha = 1e-4,
+                           size = 200,
                            p.train = 0.8,
+                           n.cor = nrow(Gna),
                            seed = NA,
                            ncores = 1) {
 
