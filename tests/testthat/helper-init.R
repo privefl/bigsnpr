274bb936dda38ac4b86b3f2565de810797aebc19
Florian Privé <florian.prive.21@gmail.com>
2019-12-09 17:19:36 +0100

    tests: omit parallel BLAS

---
+++ tests/testthat/helper-init.R
@@ -0,0 +1,19 @@
+################################################################################
+
+library(testthat)
+library(bigsnpr)
+library(bigparallelr)
+
+################################################################################
+
+opt.save <- options(bigstatsr.check.parallel.blas = FALSE)
+
+################################################################################
+
+# https://github.com/hadley/testthat/issues/567
+Sys.unsetenv("R_TESTS")
+
+not_cran <- identical(Sys.getenv("BIGSNPR_CRAN"), "false")
+NCORES <- `if`(not_cran, 2, 1)
+
+################################################################################
--- tests/testthat/test-parallel.R
+++ tests/testthat/test-parallel.R
@@ -2,12 +2,6 @@
 
 context("PARALLEL")
 
-# https://github.com/hadley/testthat/issues/567
-Sys.unsetenv("R_TESTS")
-
-not_cran <- identical(Sys.getenv("BIGSNPR_CRAN"), "false")
-NCORES <- `if`(not_cran, 2, 1)
-
 ################################################################################
 
 test <- snp_attachExtdata()
