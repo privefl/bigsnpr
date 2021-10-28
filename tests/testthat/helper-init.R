9082ec5ab108e7bcf9ea6f9b9db89d7e73686c38
Florian Privé <florian.prive.21@gmail.com>
2021-10-28 15:40:55 +0200

    fix problem with multiprocesses in tests

--- tests/testthat/helper-init.R
+++ tests/testthat/helper-init.R
@@ -13,6 +13,7 @@ options(bigstatsr.check.parallel.blas = FALSE)
 Sys.unsetenv("R_TESTS")
 
 is_cran <- !identical(Sys.getenv("BIGSNPR_CRAN"), "false")
-NCORES <- `if`(!is_cran && (parallel::detectCores() > 2), 2, 1)
+NCORES <- `if`(!is_cran && (parallel::detectCores() > 2) &&
+                 identical(Sys.getenv("_R_CHECK_LIMIT_CORES_"), "false"), 2, 1)
 
 ################################################################################
