3937989fc6d294034906adffef3c000c56802f75
Florian Privé <florian.prive.21@gmail.com>
2024-01-09 15:55:02 +0100

    fix perm

--- tmp-tests/test-reorder2.R
+++ tmp-tests/test-reorder2.R
@@ -21,7 +21,7 @@ source_cpp_url("https://raw.githubusercontent.com/privefl/bigsnpr/master/tmp-tes
 source_cpp_url("https://raw.githubusercontent.com/privefl/bigsnpr/master/tmp-tests/test-qapSA.cpp")
 n <- ncol(corr0_sq)
 get_score(corr0_sq@p, corr0_sq@i, corr0_sq@x, 1:n)         # 8682899
-perm <- qapSA(outer(1:n, 1:n, '-')^2, as.matrix(corr0_sq), 1:n - 1L)
+perm <- qapSA(outer(1:n, 1:n, '-')^2, as.matrix(corr0_sq), 1:n - 1L) + 1L
 get_score(corr0_sq@p, corr0_sq@i, corr0_sq@x, order(perm)) # 6492697
 
 perm3 <- qap::qap(outer(1:n, 1:n, '-')^2, as.matrix(corr0_sq))
@@ -30,6 +30,7 @@ get_score(corr0_sq@p, corr0_sq@i, corr0_sq@x, order(perm3)) # 10212726
 
 
 source_cpp_url("https://raw.githubusercontent.com/privefl/bigsnpr/master/tmp-tests/test-reorder2.cpp")
+# tries to make some permutation i <--> j
 reorder2(corr0_sq, max_dist = 500, sample_index = TRUE, sample_best = TRUE)
 reorder2(corr0_sq, max_dist = 500, sample_index = TRUE, sample_best = FALSE)
 reorder2(corr0_sq, max_dist = 500, sample_index = FALSE, sample_best = TRUE)
@@ -40,14 +41,14 @@ pos2 <- reorder2(corr0_sq, max_dist = 500, sample_index = TRUE, sample_best = TR
 
 
 source_cpp_url("https://raw.githubusercontent.com/privefl/bigsnpr/master/tmp-tests/test-reorder3.cpp")
+# tries to move i to some other position
 pos3 <- reorder3(corr0_sq, max_dist = 50)
 get_score(corr0_sq@p, corr0_sq@i, corr0_sq@x, pos3) # 6540525
 
-ord <- perm
 ord2 <- order(pos2)
 ord3 <- order(pos3)
 library(latticeExtra)
 c(before = Matrix::image(corr0_sq),
-  after_qapSA = Matrix::image(corr0_sq[ord, ord]),
+  after_qapSA = Matrix::image(corr0_sq[perm, perm]),
   after_reorder2 = Matrix::image(corr0_sq[ord2, ord2]),
   layout = c(3, 1))
